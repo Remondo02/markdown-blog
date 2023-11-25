1c498f8c053a7859ee32aabf923599a948456c29
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-11-25 17:11:01 +0900

    Create delete route and form

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const Article = require("./models/articles.js")
 const connectDB = require("./config/db.js")
 const articleRouter = require("./routes/articles.js")
 const { configDotenv } = require("dotenv")
+const methodOverride = require("method-override")
 const app = express()
 
 configDotenv({ path: "./config/config.env" })
@@ -12,6 +13,7 @@ connectDB()
 app.set("view engine", "ejs")
 
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method"))
 
 app.get("/", async (req, res) => {
   const articles = await Article.find().sort({ createdAt: "desc" })
