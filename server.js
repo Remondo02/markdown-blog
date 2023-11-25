1d977a1f35c032696c41778c0eb16a0aac827bbc
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-11-25 11:37:30 +0900

    HTML and fake data for index page

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 // const express = require('express')
-const express = require('express')
-const articleRouter = require('./routes/articles.js')
+const express = require("express")
+const articleRouter = require("./routes/articles.js")
 const app = express()
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs")
 
-app.use('/articles', articleRouter)
+app.use("/articles", articleRouter)
 
-app.get('/', (req, res) => {
-    res.render('index', {text: 'Hello'})
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description: "Test description",
+    },
+    {
+      title: "Test Article 2",
+      createdAt: new Date(),
+      description: "Test description 2",
+    },
+  ]
+  res.render("index", { articles: articles })
 })
 
 app.listen(5500)
