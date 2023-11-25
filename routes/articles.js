946621bd3248cee120cb06b36ef29e137d158479
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-11-25 18:17:42 +0900

    Cleanup and fix html elements order

--- routes/articles.js
+++ routes/articles.js
@@ -43,9 +43,9 @@ router.delete("/:id", async (req, res) => {
 function saveArticleAndRedirect(path) {
   return async (req, res) => {
     let article = req.article
-    ;(article.title = req.body.title),
-      (article.description = req.body.description),
-      (article.markdown = req.body.markdown)
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
     try {
       article = await article.save()
       res.redirect(`/articles/${article.slug}`)
